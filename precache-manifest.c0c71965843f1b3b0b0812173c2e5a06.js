ca24738cd8af287c455b1385a7d782c41bd1fb53
Felipe Muller <felipeavmuller@gmail.com>
2021-11-08 18:51:24 -0300

    Updates

---
+++ precache-manifest.c0c71965843f1b3b0b0812173c2e5a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac94c3fdcef481ebce8198fbc06b8dbc",
+    "url": "/expenses-project/index.html"
+  },
+  {
+    "revision": "5188a9e5032ba017b2f0",
+    "url": "/expenses-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e6f203964976501c5b0a",
+    "url": "/expenses-project/static/js/2.4d1f07b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expenses-project/static/js/2.4d1f07b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5188a9e5032ba017b2f0",
+    "url": "/expenses-project/static/js/main.d23d1aa8.chunk.js"
+  },
+  {
+    "revision": "2836ab69e51ae8d00820",
+    "url": "/expenses-project/static/js/runtime-main.e2993abe.js"
+  }
+]);

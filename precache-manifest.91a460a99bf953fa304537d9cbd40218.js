7ae321453519caeba326e9605465aa0811041ae5
Felipe Muller <felipeavmuller@gmail.com>
2021-11-08 19:09:28 -0300

    Updates

---
+++ precache-manifest.91a460a99bf953fa304537d9cbd40218.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7026eef1477b2b4a3c122990fc5378d0",
+    "url": "/expenses-wallet-project/index.html"
+  },
+  {
+    "revision": "bbf8029214334582a463",
+    "url": "/expenses-wallet-project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1dccb3e56cd2094cf514",
+    "url": "/expenses-wallet-project/static/js/2.5ceadd57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/expenses-wallet-project/static/js/2.5ceadd57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf8029214334582a463",
+    "url": "/expenses-wallet-project/static/js/main.6cfe8873.chunk.js"
+  },
+  {
+    "revision": "5fb3b6dc5e5df5edf193",
+    "url": "/expenses-wallet-project/static/js/runtime-main.34897ab6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/expenses-project/precache-manifest.8af26eeece76911c3591ac6e0bc933af.js"
+  "/expenses-wallet-project/precache-manifest.91a460a99bf953fa304537d9cbd40218.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/expenses-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/expenses-wallet-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
